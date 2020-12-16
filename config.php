56e1a6d8610237b9363a32b31102b7fd38130e23
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-16 13:31:03 -0500

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//for everyone else fill in these variable strings with the connection
-//details from your respective servers and delete the Heroku block above
-$dbhost = "sql1.njit.edu";//this should be fine for all but NJIT, which uses "sql1.njit.edu" or "sql2.njit.edu" or "sql3.njit.edu"
-$dbuser = "cba7";//your ucid since we follow this pattern
-$dbpass = "Blueblue49!";//database password generated or chosen per respective steps
-$dbdatabase = "cba7";//your ucid since we follow this pattern
-?>
