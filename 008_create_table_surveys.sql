028e481327c6f45f2de1574a4c12351eb9ab72f5
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-17 15:15:25 -0500

    Update 008_create_table_surveys.sql

--- 008_create_table_surveys.sql
+++ 008_create_table_surveys.sql
@@ -1,12 +1,14 @@
-CREATE TABLE Survey
+CREATE TABLE IF NOT EXISTS F20_Surveys
 (
-    id          int auto_increment,
-    title       varchar(30) not null unique,
-    description TEXT,
-    visibility  int, -- Draft 0, Private 1, Public 2
-    created TIMESTAMP default CURRENT_TIMESTAMP,
-    modified TIMESTAMP default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-    user_id int,
+    id               int auto_increment,
+    name             varchar(30) not null,
+    description      text,
+    attempts_per_day int                  default 1,
+    use_max          tinyint              default 0,
+    max_attempts     int                  default 1,
+    modified         TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP on update current_timestamp,
+    created          TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    user_id          int,
     primary key (id),
     FOREIGN KEY (user_id) REFERENCES Users (id)
 )
