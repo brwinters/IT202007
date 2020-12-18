c2ae324755fc9c649c99b9f3ef76a47e15de1337
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-17 22:15:13 -0500

    Update create_survey.php

--- create_survey.php
+++ create_survey.php
@@ -1,10 +1,4 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
-<?php
-if (!has_role("Admin")) {
-    //this will redirect to login and kill the rest of this script (prevent it from executing)
-    flash("You don't have permission to access this page");
-    die(header("Location: "/login.php"));
-}
 ?>
 <div class="container-fluid">
         <h3>Create Survey</h3>
