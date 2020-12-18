f5d7f6af769ce94e0095ac2a5c598928a2c67a38
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-17 19:08:10 -0500

    Update create_survey.php

--- create_survey.php
+++ create_survey.php
@@ -26,7 +26,50 @@ if (isset($_POST["submit"])) {
         if (isset($_POST["use_max"]) && $_POST["use_max"] == 'on') {
             $use_max = true;
         }
-    }
+        if (is_numeric($attempts_per_day) && (int)$attempts_per_day > 0) {
+            $attempts_per_day = (int)$attempts_per_day;
+        }
+        else {
+            $is_valid = false;
+            flash("Attempts per day must be a numerical value greater than zero", "danger");
+        }
+        $max_attempts = $_POST["max_attempts"];
+        if (is_numeric($max_attempts) && (int)$max_attempts >= 0) {
+            $max_attempts = (int)$max_attempts;
+        }
+        else {
+            $is_valid = false;
+            flash("Max attempts must be a numerical value greater than or equal to zero, even if not used", "danger");
+        }
+        if ($is_valid) {
+            //TODO here's where it gets a tad hacky and there are better ways to do it.
+            $index = 0;
+            $assumed_max_questions = 100;//this isn't a realistic limit, it's just to ensure
+            $questions = [];
+            //we don't get stuck in an infinite loop since while(true) is dangerous if not handled appropriately
+            for ($index = 0; $index < $assumed_max_questions; $index++) {
+                $question = false;
+                if (isset($_POST["question_$index"])) {
+                    $question = $_POST["question_$index"];
+                }
+                if ($question) {
+                    $assumed_max_answers = 100;//same as $assumed_max_questions (var sits here so it resets each loop)
+                    $answers = [];//reset array each loop
+                    for ($i = 0; $i < $assumed_max_answers; $i++) {
+                        $check = "" . join(["question_", $index, "_answer_", $i]);
+                        error_log("Checking for pattern $check");
+                        $answer = false;
+                        if (isset($_POST[$check])) {
+                            $answer = $_POST[$check];
+                        }
+                        if ($answer) {
+                            array_push($answers, ["answer" => $answer]);
+                        }
+                        else {
+                            //we can break this loop since we have no more answers to parse
+                            break;
+                        }
+                    }
                     array_push($questions, [
                         "question" => $question,
                         "answers" => $answers
@@ -85,7 +128,48 @@ function save_questionnaire($questionnaire) {
         ":um" => $questionnaire["use_max"] ? "1" : "0",//Convert to 1 or 0 for insertion as tinyint 
         ":user_id" => get_user_id()
     ]);
- 
+    if ($r) {//insert questions
+        $survey_id = $db->lastInsertId();
+        //we could bulk insert questions, but it'll be a bit complex to get the ids back out
+        //for use in the Answers insert, so instead I'll do a less efficient route and insert a question and its
+        //answers one at a time.
+        //loop over each question, insert the question and respective answers
+        foreach ($questionnaire["questions"] as $questionIndex => $q) {
+            $stmt = $db->prepare("INSERT INTO F20_Questions(question, survey_id) VALUES (:q, :survey_id)");
+            //echo "<pre>" .var_export($q, true) . "</pre>";
+            $r = $stmt->execute([":q" => $q["question"], ":survey_id" => $survey_id]);
+            if ($r) {//insert answers
+                $question_id = $db->lastInsertId();
+                $query = "INSERT INTO F20_Answers(answer, question_id) VALUES";
+                $params = [];
+                foreach ($q["answers"] as $answerIndex => $a) {
+                    if ($answerIndex > 0) {
+                        $query .= ",";
+                    }
+                    $query .= "(:a$answerIndex, :qid)";
+                    $params[":a$answerIndex"] = $a["answer"];
+                }
+                //only need to map this once since it's the same for this batch of answers
+                $params[":qid"] = $question_id;
+                //echo "<br>Answer<br>";
+                //echo $query;
+                //echo var_export($params, true);
+                $stmt = $db->prepare($query);
+                $r = $stmt->execute($params);
+                if (!$r) {
+                    $hadError = true;
+                    flash("Error creating answers: " . var_export($stmt->errorInfo(), true), "danger");
+                }
+            }
+            else {
+                $hadError = true;
+                flash("Error creating questions: " . var_export($stmt->errorInfo(), true), "danger");
+            }
+        }
+    }
+    else {
+        $hadError = true;
+        flash("Error creating survey: " . var_export($stmt->errorInfo(), true), "danger");
     }
     if (!$hadError) {
         flash("Successfully created Survey: " . $questionnaire["name"], "success");
@@ -94,6 +178,60 @@ function save_questionnaire($questionnaire) {
     }
 }
 
+?>
+    <div class="container-fluid">
+        <form method="POST">
+            <div class="form-group">
+                <label for="questionnaire_name">Questionnaire Name</label>
+                <input class="form-control" type="text" id="questionnaire_name" name="questionnaire_name" required/>
+            </div>
+            <div class="form-group">
+                <label for="questionnaire_desc">Questionnaire Description</label>
+                <textarea class="form-control" type="text" id="questionnaire_desc" name="questionnaire_desc"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="attempts_per_day">Attempts per day</label>
+                <input class="form-control" type="number" id="attempts_per_day" name="attempts_per_day" value="1"
+                       min="1"/>
+            </div>
+            <div class="form-group">
+                <label for="max_attempts">Max Attempts</label>
+                <input class="form-control" type="number" id="max_attempts" name="max_attempts" value="1" min="0"/>
+            </div>
+            <div class="form-group">
+                <label for="use_max">Use Max?</label>
+                <input class="form-control" type="checkbox" id="use_max" name="use_max"/>
+            </div>
+            <div class="list-group">
+                <div class="list-group-item">
+                    <div class="form-group">
+                        <label for="question_0">Question</label>
+                        <input class="form-control" type="text" id="question_0" name="question_0" required/>
+                    </div>
+                    <button class="btn btn-danger" onclick="event.preventDefault(); deleteMe(this);">X</button>
+                    <div class="list-group">
+                        <div class="list-group-item">
+                            <div class="form-group">
+                                <label for="question_0_answer_0">Answer</label>
+                                <input class="form-control" type="text" id="question_0_answer_0"
+                                       name="question_0_answer_0"
+                                       required/>
+                            </div>
+                            <button class="btn btn-danger" onclick="event.preventDefault(); deleteMe(this);">X</button>
+                        </div>
+                    </div>
+                    <button class="btn btn-secondary" onclick="event.preventDefault(); cloneThis(this);">Add Answer
+                    </button>
+                </div>
+            </div>
+            <button class="btn btn-secondary" onclick="event.preventDefault(); cloneThis(this);">Add Question</button>
+
+            <div class="form-group">
+                <input type="submit" name="submit" class="btn btn-primary" value="Create Questionnaire"/>
+            </div>
+        </form>
+    </div>
+    <script>
         function update_names_and_ids($ele) {
             let $lis = $ele.children(".list-group-item");
             //loop over all list-group-items of list-group
