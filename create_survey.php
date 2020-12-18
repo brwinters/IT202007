1d09fe069a02630a80280dedd937d877b18a491a
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-17 20:08:16 -0500

    Update create_survey.php

--- create_survey.php
+++ create_survey.php
@@ -12,3 +12,28 @@ require_once(__DIR__ . "/../lib/helpers.php");
     <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>
 </ul>
+<?php
+/*put this at the bottom of the page so any templates
+ populate the flash variable and then display at the proper timing*/
+?>
+<div class="container" id="flash">
+    <?php $messages = getMessages(); ?>
+    <?php if ($messages): ?>
+        <?php foreach ($messages as $msg): ?>
+            <div class="row justify-content-center">
+                <div class="alert alert-primary" role="alert"><?php echo $msg; ?></div>
+            </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</div>
+<script>
+    //used to pretend the flash messages are below the first nav element
+    function moveMeUp(ele) {
+        let target = document.getElementsByTagName("nav")[0];
+        if (target) {
+            target.after(ele);
+        }
+    }
+
+    moveMeUp(document.getElementById("flash"));
+</script>
