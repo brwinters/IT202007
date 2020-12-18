e020f0096c71a3cdb5126832dfc58949d03cba2d
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-17 21:03:11 -0500

    Update list_surveys.php

--- list_surveys.php
+++ list_surveys.php
@@ -40,7 +40,6 @@
         </div>
     </div>
 <?php require(__DIR__ . "/../partials/flash.php");
-<?php
 //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
