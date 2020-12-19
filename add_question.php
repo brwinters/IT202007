4a7d0d6768a731f7949bcf01a8548df8c15ab181
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-19 17:41:19 -0500

    Update add_question.php

--- add_question.php
+++ add_question.php
@@ -9,9 +9,11 @@ if (isset($_GET["id"])) {
 
 //fetching
 $result = [];
-if (isset($_POST)) {
+if (isset($id)) {
  $stmt = $db->prepare("INSERT INTO Survey (title, description) VALUES(:name, :user)");
    }
+<FORM NAME ="form1" METHOD =$_POST
+	Enter a question: <INPUT TYPE = 'TEXT' Name ='question'  value="What is the Question?"  maxlength="40">
 ?>
 	<INPUT TYPE = "Submit" Name = "Sub1"  VALUE = "Set this Question">
 <?php require(__DIR__ . "/partials/flash.php");
