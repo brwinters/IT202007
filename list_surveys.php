10db1040d3da93ac98b2223b9d25ce610732aca2
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-18 19:35:22 -0500

    Update list_surveys.php

--- list_surveys.php
+++ list_surveys.php
@@ -1,4 +1,4 @@
-<?php require_once(__DIR__ . "/../partials/nav.php"); ?>
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
 
 <?php
 $query = "";
@@ -55,4 +55,4 @@ if (isset($_POST["search"]) && !empty($query)) {
             <?php endif; ?>
         </div>
     </div>
-<?php require(__DIR__ . "/../partials/flash.php");
+<?php require(__DIR__ . "/partials/flash.php");
