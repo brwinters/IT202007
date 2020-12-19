7d26600b9868018a175c74be4d40756c46a68eae
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-18 21:12:27 -0500

    Update list_surveys.php

--- list_surveys.php
+++ list_surveys.php
@@ -30,12 +30,12 @@ if (isset($_POST["search"]) && !empty($query)) {
                     <?php foreach ($results as $r): ?>
                         <div class="list-group-item">
                             <div class="row">
-                                <div class="col">
+                               
                                     <div>Title:</div>
                                     <div><?php safer_echo($r["title"]); ?></div>
                                 </div>
                              div class="col">
-                                    <div>Title:</div>
+                                    <div>Description:</div>
                                     <div><?php safer_echo($r["description"]); ?></div>
                                 </div>
                                 <div class="col">
