7f7d9c905b6e04b26b1f56f3f3ba04f7c431c9c6
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-19 20:16:29 -0500

    Update add_question.php

--- add_question.php
+++ add_question.php
@@ -1,20 +1,30 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
-//we'll put this at the top so both php block have access to it
+$survey_id = -1
 if (isset($_GET["id"])) {
-    $id = $_GET["id"];
+    $survey_id = $_GET["id"];
 }
 
-
-//fetching
-$result = [];
-if (isset($id)) {
- $stmt = $db->prepare("INSERT INTO Survey (title, description) VALUES(:name, :user)");
+if (isset($_POST["submit"])) {
+	$question = $_POST["question"];
+	if(!empty(question) && $survey_id > -1
+ $stmt = $db->prepare("INSERT INTO Questions (surevy_id, question) VALUES(:sid, :q)");
+	$r = $stmt->execute ([":sid"=>$survey_id, ":q"=>$question]);
+	   if($r){
+		   flash("Question created");
    }
-?>
-   <FORM NAME ="form1" METHOD =$_POST
-	Enter a question: <INPUT TYPE = 'TEXT' Name ='question'  value="What is the Question?"  maxlength="40">
-
-
-	<INPUT TYPE = "Submit" Name = "Sub1"  VALUE = "Set this Question">
-		</form>
+	   else{
+		   flash("Problem creating question " . var_export($stmt->errorINfo(), true));
+	   }
+	   }
+	   else{
+		   flash("Questions must not be empty and survey id must be passed in the url");
+	   }
+	   }
+	   ?>
+<form method="POST">
+	<input name="questions"/>
+	<input type="submit" name="submit"/>
+	<form>
+  
 <?php require(__DIR__ . "/partials/flash.php");
