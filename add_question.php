fc6dacdc7d7eccd4988249865b05046dccb8230c
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-21 18:23:03 -0500

    Update add_question.php

--- add_question.php
+++ add_question.php
@@ -8,7 +8,7 @@ if (isset($_GET["id"])) {
 if (isset($_POST["submit"])) {
 	$question = $_POST["question"];
 	if(!empty($question) && $survey_id > -1){
- $stmt = $db->prepare("INSERT INTO Questions (surevy_id, question) VALUES(:sid, :q)");
+ $stmt = $db->prepare("INSERT INTO F20_Questions (surevy_id, question) VALUES(:sid, :q)");
 	$r = $stmt->execute ([":sid"=>$survey_id, ":q"=>$question]);
 	   if($r){
 		   flash("Question created");
