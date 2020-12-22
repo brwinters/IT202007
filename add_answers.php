96581f3ad83925c91d4990e88733c4867ebfe633
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-21 19:19:03 -0500

    Create add_answers.php

---
+++ add_answers.php
@@ -0,0 +1,30 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+$question_id = -1;
+if (isset($_GET["id"])) {
+    $question_id = $_GET["id"];
+}
+
+if (isset($_POST["submit"])) {
+	$answer = $_POST["answer"];
+	if(!empty($answer) && $question_id > -1){
+	$db = getDB();
+ $stmt = $db->prepare("INSERT INTO F20_Answers (question_id, answer) VALUES(:sid, :q)");
+	$r = $stmt->execute ([":sid"=>$question_id, ":q"=>$answer]);
+	   if($r){
+		   flash("Answers created");
+   }
+	   else{
+		   flash("Problem creating answer " . var_export($stmt->errorINfo(), true));
+	   }
+	   }
+	   else{
+		   flash("Answer must not be empty and survey id must be passed in the url");
+	   }
+	   }
+	   ?>
+<form method="POST">
+	<input name="answer"/>
+	<input type="submit" name="submit"/>
+	<form>
+<?php require(__DIR__ . "/partials/flash.php");
