de5e80633ba85037b156dc9b2bef1c261c60c597
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-21 22:21:40 -0500

    Update take_survey.php

--- take_survey.php
+++ take_survey.php
@@ -9,7 +9,7 @@ if (isset($_GET["id"])) {
 $result = [];
 if (isset($id)) {
     $db = getDB();
-    $stmt = $db->prepare("SELECT Eggs.id,name,state,base_rate,mod_min,mod_max,next_stage_time, user_id, Users.username FROM F20_Eggs as Eggs JOIN Users on Eggs.user_id = Users.id where Eggs.id = :id");
+    $stmt = $db->prepare("SELECT survey.id,title,description,question_id,answer_id, user_id, Users.username FROM Survey as Survey JOIN Users on Survey.user_id = Users.id where Survey.id = :id");
     $r = $stmt->execute([":id" => $id]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     if (!$result) {
@@ -18,7 +18,7 @@ if (isset($id)) {
     }
 }
 ?>
-    <h3>View Egg</h3>
+    <h3>Test Survey</h3>
 <?php if (isset($result) && !empty($result)): ?>
     <div class="card">
         <div class="card-title">
@@ -27,11 +27,10 @@ if (isset($id)) {
         <div class="card-body">
             <div>
                 <p>Stats</p>
-                <div>Rate: <?php safer_echo($result["base_rate"]); ?></div>
-                <div>Modifier: <?php safer_echo($result["mod_min"]); ?> - <?php safer_echo($result["mod_max"]); ?></div>
-                <div>Current State: <?php getState($result["state"]); ?></div>
-                <div>Next Stage: <?php safer_echo($result["next_stage_time"]); ?></div>
-                <div>Owned by: <?php safer_echo($result["username"]); ?></div>
+                <div>Title: <?php safer_echo($result["title"]); ?></div>
+                <div>Description: <?php safer_echo($result["description"]); ?></div>
+                <div>Status: <?php getState($result["status"]); ?></div>
+
             </div>
         </div>
     </div>
