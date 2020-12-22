9e3e2301a82c1e61872dfdab8c51ef2a4826e325
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-22 18:46:35 -0500

    Update take_survey.php

--- take_survey.php
+++ take_survey.php
@@ -9,7 +9,7 @@ if (isset($_GET["id"])) {
 $results = [];
 if (isset($id)) {
     $db = getDB();
-    $stmt = $db->prepare("SELECT id,question,answer FROM F20_Questions JOIN F20_Answers ON F20_Answers.question_id = F20_Questions.id WHERE survey_id = :id");
+    $stmt = $db->prepare("SELECT id,F20_Question,answer FROM F20_Questions JOIN F20_Answers ON F20_Answers.question_id = F20_Questions.id WHERE survey_id = :id");
     $r = $stmt->execute([":id" => $id]);
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     if (!$results) {
