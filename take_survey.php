4cebfad0d6dd317e9c6b8fc1bd37fb68ca6b9bac
brwinters <70977727+brwinters@users.noreply.github.com>
2020-12-22 21:04:39 -0500

    Update take_survey.php

--- take_survey.php
+++ take_survey.php
@@ -1,5 +1,42 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
+if (isset($_POST["submit"])) {
+    echo "<pre>" . var_export($_POST, true) . "</pre>";
+    $survey_id = $_GET["id"];
+    $user_id = get_user_id();
+    $params = [];
+    $query = "INSERT INTO F20_Responses (survey_id, question_id, answer_id, user_id) VALUES";//ignore sql error hint
+    $i = 0;//can't use $key here since it presents a question_id and will always be > 0, so using a temp var to count
+    foreach ($_POST as $key => $item) {
+        if (is_numeric($key)) {
+            //assuming this is question id
+            //assuming value is answer id
+            if ($i > 0) {
+                $query .= ",";
+            }
+            $query .= "(:sid, :q$i, :a$i, :uid)";
+            $params[":q$i"] = $key;
+            $params[":a$i"] = $item;
+        }
+        $i++;
+    }
+    $params[":sid"] = $survey_id;
+    $params[":uid"] = $user_id;
+    $db = getDB();
+    $stmt = $db->prepare($query);
+    $r = $stmt->execute($params);
+    if ($r) {
+        flash("Answers have been recorded", "success");
+    }
+    else {
+        flash("There was an error recording your answers: " . var_export($stmt->errorInfo(), true), "danger");
+    }
+    die(header("Location: " . getURL("surveys.php")));
+}
+?>
+<input type="hidden" name="question_id" value="<?php echo $r["question_id"];?>"/>
+
+<?php
 if (isset($_GET["id"])) {
     $id = $_GET["id"];
 }
@@ -27,7 +64,7 @@ if (isset($id)) {
                     <?php foreach ($results as $r): ?>
                         <div class="list-group-item">
                                     <div>Question:</div>
-                                    <div><?php echo($r["question"]); ?></div>
+                                    <div><?php safer_echo($r["question"]); ?></div>
                                 </div> 
                      <?php endforeach; ?>
                      <?php foreach ($results as $r): ?>
